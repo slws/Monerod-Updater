c6a12fafc30597413ddd3b3a322dfef3d154a45c
Simon <slws@users.noreply.github.com>
2020-11-29 22:40:39 +0000

    Update autoupdate.ps1

--- autoupdate.ps1
+++ autoupdate.ps1
@@ -15,41 +15,38 @@ $subfolder = "$fPath\monero-cli"
 $monerod = (Get-Process -name monerod -EA SilentlyContinue).id
 $hMatch = $false
 $pgpSig = ""
-$update = [string] (& $subfolder\monerod.exe update check)
-If ($update -like "*No update available"){
-    Write-Output "No updates required."
-} ElseIf ($update -ne $null) {
-    If ($monerod -ne $null){
+If ($monerod){
+    $update = [string] (& $subfolder\monerod.exe update check)
+    If ($update -like "*No update available"){
+        Write-Output "No updates required."
+    } Else {
         $updateInfo = $update -split '\s+'
         $url = $updateInfo[10].Substring(0,$updateInfo[10].Length-1)
-        $hash = $updateInfo[12]
         $fileInfo = $url -split '/'
         $fileName = $fileInfo[4]
         Write-Output "Downloading $fileName from the web..."
         $progresspreference = 'silentlyContinue'
-        Invoke-WebRequest $url -OutFile $fPath\$fileName
+        Invoke-WebRequest $url -OutFile $fPath\Monero.zip
         $progressPreference = 'Continue'
-        $certUtilOutput = (certUtil -hashfile $fPath\$fileName SHA256) -split '\s+'
-        $fileHash = $certUtilOutput[4]
-        If ($fileHash -eq $hash){
+        If (VerifyHash($fPath)){
             Write-Output "Hash match confirmed. Killing monerod and Extracting download..."
             Stop-Process -id $monerod | Wait-Process
-            Expand-Archive -LiteralPath $fPath\$fileName -DestinationPath $fPath -Force
+            Expand-Archive -LiteralPath $fPath\Monero.zip -DestinationPath $fPath -Force
             $varName = "monero-x86_64-w64-mingw32-v"+$fileName.Substring(16,$fileName.Length-20)
             If (test-path $subfolder) {
                 Move-Item $fPath\$varName\* $subfolder\ -Force
                 Remove-Item $fPath\$varName
+                Remove-Item $fPath\monero.zip
             } Else {
                 Rename-Item $fPath\$varName $subfolder
             }
             Write-Output "Restarting monerod.exe..."
             Start-Process $subfolder\monerod.exe -ArgumentList "$lmdb $prune"
         } Else {
-            Write-Output "Hashes do not match!"
-            Write-Output "Daemon Hash: $hash"
-            Write-Output "File Hash: $fileHash"
+            Write-Output "Hashes/PGP Signature unverifiable! Follow: https://www.getmonero.org/resources/user-guides/verification-windows-beginner.html"
         }
-    } Else {
+    }
+} Else {
         Write-Output "Daemon not running."
         If (Test-Path "$fPath\monero-cli\monerod.exe"){
             Write-Output "Starting monerod.exe..."
@@ -58,41 +55,38 @@ If ($update -like "*No update available"){
             Write-Output "Monerod not found. Downloading latest from the web..."
             $url = "https://downloads.getmonero.org/cli/win64"
             $progresspreference = 'silentlyContinue'
-            Invoke-WebRequest $url -OutFile $fPath\$fileName
+            Invoke-WebRequest $url -OutFile $fPath\Monero.zip
             $progressPreference = 'Continue'
-            $certUtilOutput = (certUtil -hashfile $fPath\$fileName SHA256) -split '\s+'
-            $fileHash = $certUtilOutput[4]
-            $hashURL = "https://www.getmonero.org/downloads/hashes.txt"
-            Invoke-WebRequest $hashURL -OutFile $fPath\hashes.txt
-            $hashTXT = Get-Content $fPath\hashes.txt
-            ForEach ($line in $hashTXT){
-                if ($line -like "*$fileHash*"){
-                    $hMatch = $true
-                }
-            }
-            if ($hMatch){
-                $pgpSig =  [string] (& gpg --textmode --verify $fPath\hashes.txt 2>&1)
-                if ($pgpSig -like "*Good signature from `"binaryFate <binaryfate@getmonero.org>*"){
-                    Write-Output "PGP Signature good."
-                    Write-Output "Hash match confirmed. Extracting download..."
-                    Expand-Archive -LiteralPath $fPath\$fileName -DestinationPath $fPath -Force
-                    $varName = "monero-x86_64-w64-mingw32-v"+$fileName.Substring(16,$fileName.Length-20)
-                    If (test-path $subfolder) {
-                        Move-Item $fPath\$varName\* $subfolder\ -Force
-                        Remove-Item $fPath\$varName
-                        Remove-Item $fPath\$fileName
-                    } Else {
-                        Rename-Item $fPath\$varName $subfolder
-                    }
-                    Write-Output "Starting monerod.exe..."
-                    Start-Process $subfolder\monerod.exe -ArgumentList "$lmdb $prune"
+            If (VerifyHash($fPath)){
+                Write-Output "Hash match confirmed. Extracting download..."
+                Expand-Archive -LiteralPath $fPath\Monero.zip -DestinationPath $fPath -Force
+                $varName = "monero-x86_64-w64-mingw32-v"+$fileName.Substring(16,$fileName.Length-20)
+                If (test-path $subfolder) {
+                    Move-Item $fPath\$varName\* $subfolder\ -Force
+                    Remove-Item $fPath\$varName
+                    Remove-Item $fPath\monero.zip
                 } Else {
-                    Write-Output $pgpSig
-                    Write-Output "PGP Signature unverifiable! https://www.getmonero.org/resources/user-guides/verification-windows-beginner.html"
+                    Rename-Item $fPath\$varName $subfolder
                 }
+                Write-Output "Starting monerod.exe..."
+                Start-Process $subfolder\monerod.exe -ArgumentList "$lmdb $prune"
             } Else {
-                Write-Output "No Hash match found, automatic update will not proceed."
+                Write-Output "Hashes/PGP Signature unverifiable! Follow: https://www.getmonero.org/resources/user-guides/verification-windows-beginner.html"
             }
         }
+}
+
+Function VerifyHash {
+Param ($fPath)
+    $match = $false
+    $hashURL = "https://www.getmonero.org/downloads/hashes.txt"
+    Invoke-WebRequest $hashURL -OutFile $fPath\hashes.txt
+    $pgpSig =  [string] (& gpg --textmode --verify $fPath\hashes.txt 2>&1)
+    If ($pgpSig -like "*Good signature from `"binaryFate <binaryfate@getmonero.org>*"){
+        $certUtilOutput = (certUtil -hashfile $fPath\monero.zip SHA256) -split '\s+'
+        If ((Get-Content $fPath\hashes.txt | %{$_ -match $certUtilOutput[4]}) -contains $true) {
+            $match = $true
+        }
     }
+    Return $match
 }
